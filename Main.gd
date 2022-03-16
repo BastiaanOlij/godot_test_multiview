48f6d376abad988dc5318b01980ccef81f7da674
Bastiaan Olij <mux213@gmail.com>
2022-03-17 10:29:34 +1100

    First commit

---
+++ Main.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var interface = XRServer.find_interface("Native mobile")
+	#if interface and interface.initialize():
+	#	get_viewport().use_xr = true;
+	pass
+
+var angle : float = 0;
+func _process(delta):
+	angle += delta
+	$RedCube.rotation.y = angle
---
+++ README.md
@@ -0,0 +1,9 @@
+# Godot 4 Multiview render test project
+
+This is a small test project to test stereoscopic/multiview rendering in Godot 4
+
+## License
+
+This project is provided under the MIT license
+
+Check license files in the asset and addons folders for 3rd party licenses.
