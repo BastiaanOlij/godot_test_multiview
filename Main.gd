d65a063c815a637df2cc3a946fa4616849884f64
Bastiaan Olij <mux213@gmail.com>
2022-03-17 10:37:43 +1100

    Fixes for latest Godot 4 and switched to desktop render

--- Main.gd
+++ Main.gd
@@ -2,10 +2,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#var interface = XRServer.find_interface("Native mobile")
-	#if interface and interface.initialize():
-	#	get_viewport().use_xr = true;
-	pass
+	var interface = XRServer.find_interface("Native mobile")
+	if interface and interface.initialize():
+		get_viewport().use_xr = true;
 
 var angle : float = 0;
 func _process(delta):
